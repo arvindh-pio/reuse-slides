cf3f39eaad037526fcfee081f3f7c359382a6c50
Arvindh Ganesan <arvindh.ganesan@programmers.io>
2025-03-21 11:34:39 +0530

    removed unused code in filters

--- src/taskpane/utils/filters.ts
+++ src/taskpane/utils/filters.ts
@@ -1,14 +1,7 @@
-export const getFilteredData = (datas, key, value, removing = false) => {
+export const getFilteredData = (datas, key, value) => {
     if (value?.length === 0) return datas;
-    if (removing) {
-        return datas?.filter((data) => {
-            const val = value?.some((val) => data?.fields[key]?.includes(val));
-            return val;
-        });
-    } else {
-        return datas?.filter((data) => {
-            const val = value?.some((val) => !data?.fields[key]?.includes(val));
-            return val;
-        });
-    }
+    return datas?.filter((data) => {
+        const val = value?.some((val) => data?.fields[key]?.includes(val));
+        return val;
+    });
 }
